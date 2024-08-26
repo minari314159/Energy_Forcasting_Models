356840fcd9d9262104a8abb6f2b5c51337cc3941
SJ <127341132+minari314159@users.noreply.github.com>
2024-08-26 16:47:50 -0400

    Move load_PJME and load_PJMW functions to utils/load_data.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,5 @@ from .clean_data import mean_absolute_percentage_error, missing_data, iqr_outlie
 from .preprocess import create_timeseries_features, create_cat_features
 
 from .plot import plot_train_test_split,  plot_time_range, plot_mean_monthly, plot_feature_importance
+
+from load_data import load_PJME, load_PJMW
